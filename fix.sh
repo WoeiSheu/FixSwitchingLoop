512b4d0ed6b1f22dec7225f239a3a68601534901
Xu Wei <xuweiiam@outlook.com>
2018-12-20 10:36:42 +0800

    Upload the shell script.

---
+++ fix.sh
@@ -0,0 +1,41 @@
+#!/usr/local/bin/bash
+
+### save old mac address
+oldmac=`ifconfig eth0 | awk '{print $2}' | sed -n '5p'`
+echo $oldmac >> oldmac.txt
+
+### find ip address
+#ips=`hostname -I`
+#iplist=($ips)
+#ip=${iplist[0]}
+ip=`ifconfig eth0 | awk '{print $2}' | sed -n '2p'`
+echo $ip
+iphead=${ip:0:3}
+
+#ifconfig eth0 up
+while [ "$iphead" != "192" ]
+do
+  ### generate new mac address
+  rand=`cat /dev/urandom | sed 's/[^a-f0-9]//g' | strings -n 2 | head -n 6`
+  #echo ${rand[@]}
+  newmac=''
+  for item in $rand
+  do
+    newmac=$newmac:$item
+  done
+  echo ${newmac:1}
+  newmac=${newmac:1}
+
+  ### change mac address
+  ifconfig eth0 down
+  ifconfig eth0 hw ether $newmac
+
+  ### wait some time to ensure that ip will change
+  sleep 6
+  ifconfig eth0 up
+  
+  ### find new ip address
+  ip=`ifconfig eth0 | awk '{print $2}' | sed -n '2p'`
+  echo $ip
+  iphead=${ip:0:3}
+done
